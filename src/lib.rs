058f42882c84a111890e0a85e046643cccd91c5e
Guy Garcia <guysebastiangarcia@gmail.com>
2022-06-12 22:29:02 -0400

    cleaned up generics and implemented a 1D example

--- src/lib.rs
+++ src/lib.rs
@@ -4,28 +4,24 @@ use std::marker::PhantomData;
 use rand::SeedableRng;
 
 /// Easy to use interface that handles the map transformations
-pub struct MarkovEngine<M: MapState<T, P, D, Slice>, T: Transformation<Slice>, P, D, Slice> {
+pub struct MarkovEngine<M: MapState<T>, T: Transformation> {
     pub state: M,
-    pub rules: Vec<Rule<T, Slice>>,
-    pos: PhantomData<P>,
-    dir: PhantomData<D>
+    pub rules: Vec<Rule<T>>,
     // TODO: implement random in the update functions
     //pub seed: Box<dyn SeedableRng>
 }
 
-impl<M: MapState<T, P, D, Slice>, T: Transformation<Slice>, P, D, Slice> MarkovEngine<M, T, P, D, Slice> {
+impl<M: MapState<T>, T: Transformation> MarkovEngine<M, T> {
     /// Initializer since we have phantom types
-    pub fn new(state: M, rules: Vec<Rule<T, Slice>>) -> Self {
+    pub fn new(state: M, rules: Vec<Rule<T>>) -> Self {
         Self {
             state,
             rules,
-            pos: Default::default(),
-            dir: Default::default()
         }
     }
 
     /// Updates the map as if it were going by steps
-    pub fn update(&mut self) -> Option<(&Rule<T, Slice>, Vec<(P, D)>)> {
+    pub fn update(&mut self) -> Option<(&Rule<T>, Vec<(M::Pos, M::Dir)>)> {
         for rule in self.rules.iter_mut() {
             // Skip rule if loops was reached
             if let Some(times) = rule.repeat {
@@ -59,9 +55,14 @@ impl<M: MapState<T, P, D, Slice>, T: Transformation<Slice>, P, D, Slice> MarkovE
 
 
 /// Stores the state of the canvas being worked on
-pub trait MapState<T: Transformation<Slice>, P, D, Slice> {
+pub trait MapState<T: Transformation> {
+    // Will be the equivalent for coordinate position
+    type Pos;
+    // Direction of coordinate
+    type Dir;
+
     /// Tries to update the map state given one rule
-    fn update(&mut self, rule: &Rule<T, Slice>) -> Option<Vec<(P, D)>> {
+    fn update(&mut self, rule: &Rule<T>) -> Option<Vec<(Self::Pos, Self::Dir)>> {
         // Get the match result according to rules
         let matching = self.matching(&rule.comp, &rule.rule_type);
 
@@ -89,7 +90,7 @@ pub trait MapState<T: Transformation<Slice>, P, D, Slice> {
     }
 
     /// Rule logic router
-    fn matching(&self, pattern: &T, rule_type: &Match) -> Option<Vec<(P, D)>> {
+    fn matching(&self, pattern: &T, rule_type: &Match) -> Option<Vec<(Self::Pos, Self::Dir)>> {
         match rule_type {
             Match::Linear => self.linear_match(pattern),
             Match::Random { tries } => self.random_match(pattern, *tries),
@@ -99,30 +100,30 @@ pub trait MapState<T: Transformation<Slice>, P, D, Slice> {
     }
 
     /// Follows a linear "left to right" finding style until it finds one item that satisfied the rule
-    fn linear_match(&self, pattern: &T) -> Option<Vec<(P, D)>>;
+    fn linear_match(&self, pattern: &T) -> Option<Vec<(Self::Pos, Self::Dir)>>;
 
     /// Randomly picks for `tries` times until it finds an item that satisfies the rule or runs out of tries
-    fn random_match(&self, pattern: &T, tries: u64) -> Option<Vec<(P, D)>>;
+    fn random_match(&self, pattern: &T, tries: u64) -> Option<Vec<(Self::Pos, Self::Dir)>>;
 
     /// Matches all items that dont conflict, if conflicting it may pick the "leftmost" item
-    fn match_all_without_conflicts(&self, pattern: &T) -> Option<Vec<(P, D)>>;
+    fn match_all_without_conflicts(&self, pattern: &T) -> Option<Vec<(Self::Pos, Self::Dir)>>;
 
     /// Matches all items, if there are conflicts then it runs a supplied superposition function on top of the conflicts
-    fn match_all(&self, pattern: &T) -> Option<Vec<(P, D)>>;
+    fn match_all(&self, pattern: &T) -> Option<Vec<(Self::Pos, Self::Dir)>>;
 
     /// Sets the pattern, given the direction and position
-    fn set(&mut self, pattern: &T, pos: &P, dir: &D);
+    fn set(&mut self, pattern: &T, pos: &Self::Pos, dir: &Self::Dir);
 }
 
 /// Individual Markov rule logic
-pub struct Rule<T: Transformation<Slice>, Slice> {
+pub struct Rule<T: Transformation> {
     pub comp: T,
     pub result: T,
     pub rule_type: Match,
-    pub repeat: Option<u64>
+    pub repeat: Option<u64>,
 }
 
-impl<T: Transformation<Slice>, Slice> Rule<T, Slice> {
+impl<T: Transformation> Rule<T> {
     /// Creates a new rule and verifies that the logic is correct
     pub fn new(comp: T, result: T, rule_type: Match, repeat: Option<u64>) -> Self {
         if !comp.equal_size(&result) {
@@ -133,7 +134,7 @@ impl<T: Transformation<Slice>, Slice> Rule<T, Slice> {
             comp,
             result,
             rule_type,
-            repeat
+            repeat,
         }
     }
 
@@ -158,23 +159,14 @@ pub enum Match {
 }
 
 /// Used to replace / compare with parts of MapState
-pub trait Transformation<Slice> {
+pub trait Transformation {
+    // Internal transformation item that will be used for map slices
+    type Item;
+
     /// Verifies that both transformations are of the same size
     /// Used for rules
     fn equal_size(&self, compared: &Self) -> bool;
 
     /// Returns the transformation
-    fn get(&self) -> &Slice;
-}
-
-/// Represents the position for MapState
-pub trait Pos<Position> {
-    /// Returns the position
-    fn get(&self) -> &Position;
-}
-
-/// Represents the direction for MapState
-pub trait Dir<Direction> {
-    /// Returns the direction
-    fn get(&self) -> &Direction;
+    fn get(&self) -> &Self::Item;
 }
--- src/markov1D.rs
+++ src/markov1D.rs
@@ -1,46 +1,142 @@
-use crate::{Dir, MapState, Transformation};
+use crate::{MapState, Transformation};
 
 pub struct MapState1D<T: Eq> {
     pub state: Vec<T>
 }
 
-impl MapState<Transformation1D<T>, Vec<u64>, Direction1D, T> for MapState1D<T> {
-    fn linear_match(&self, pattern: &T) -> Option<Vec<(P, D)>> {
-        todo!()
-    }
-
-    fn random_match(&self, pattern: &T, tries: u64) -> Option<Vec<(P, D)>> {
-        todo!()
-    }
-
-    fn match_all_without_conflicts(&self, pattern: &T) -> Option<Vec<(P, D)>> {
-        todo!()
-    }
-
-    fn match_all(&self, pattern: &T) -> Option<Vec<(P, D)>> {
-        todo!()
-    }
-
-    fn set(&mut self, pattern: &T, pos: &P, dir: &D) {
-        todo!()
+impl<T: Eq> MapState1D<T> {
+    pub fn new(state: Vec<T>) -> Self {
+        Self {
+            state
+        }
     }
 }
 
-pub struct Transformation1D<T: Eq> {
+impl<T: Eq + Clone> MapState<Transformation1D<T>> for MapState1D<T> {
+    type Pos = usize;
+    type Dir = Direction1D;
+
+    fn linear_match(&self, pattern: &Transformation1D<T>) -> Option<Vec<(Self::Pos, Self::Dir)>> {
+        let item_pattern = pattern.get();
+
+        'state_iter: for i in 0..(self.state.len() + 1 - item_pattern.len()) {
+            for n in 0..item_pattern.len() {
+                if self.state[i+n] != item_pattern[n] {
+                    continue 'state_iter
+                }
+            }
+            return Some(vec![(i, Self::Dir::Right)])
+        }
+
+        None
+    }
+
+    fn random_match(&self, pattern: &Transformation1D<T>, tries: u64) -> Option<Vec<(Self::Pos, Self::Dir)>> {
+        todo!()
+    }
+
+    fn match_all_without_conflicts(&self, pattern: &Transformation1D<T>) -> Option<Vec<(Self::Pos, Self::Dir)>> {
+        todo!()
+    }
+
+    fn match_all(&self, pattern: &Transformation1D<T>) -> Option<Vec<(Self::Pos, Self::Dir)>> {
+        todo!()
+    }
+
+    fn set(&mut self, pattern: &Transformation1D<T>, pos: &Self::Pos, dir: &Self::Dir) {
+        // NOTE: Dir not used since its traditional 1D markov
+        self.state.splice(*pos..(pos+pattern.item.len()), pattern.item.to_vec());
+    }
+}
+
+pub struct Transformation1D<T: Eq + Clone> {
     pub item: Vec<T>
 }
 
-impl<T: Eq> Transformation<T> for Transformation1D<T> {
+impl<T: Eq + Clone> Transformation1D<T> {
+    pub fn new(item: Vec<T>) -> Self {
+        Self {
+            item
+        }
+    }
+}
+
+impl<T: Eq + Clone> Transformation for Transformation1D<T> {
+    type Item = Vec<T>;
+
     fn equal_size(&self, compared: &Self) -> bool {
-        self.item == compared.item
+        self.item.len() == compared.item.len()
     }
 
-    fn get(&self) -> &T {
+    fn get(&self) -> &Self::Item {
         &self.item
     }
 }
 
 pub enum Direction1D {
-    Left,
     Right
+}
+
+#[cfg(test)]
+mod test_1d {
+    use crate::{MapState, MarkovEngine, Match, Rule};
+    use crate::markov1D::{Direction1D, MapState1D, Transformation1D};
+
+    #[test]
+    fn init_state() {
+        let map = MapState1D::new(String::from("testing").chars().collect());
+
+        assert_eq!(map.state, vec!['t','e','s','t','i','n','g']);
+    }
+
+    #[test]
+    fn linear_find() {
+        let map = MapState1D::new(String::from("testing").chars().collect());
+
+        let find = map.linear_match(&Transformation1D {
+            item: vec!['x']
+        });
+
+        assert!(find.is_none());
+
+        let find = map.linear_match(&Transformation1D {
+            item: vec!['s']
+        });
+
+        assert!(find.is_some());
+        assert_eq!(2, find.unwrap()[0].0);
+    }
+
+    #[test]
+    fn linear_rules() {
+        let map = MapState1D::new(String::from("testing").chars().collect());
+        // TODO: impl macros to make creation these transformation types easier
+        let rules = vec![
+            Rule::new(
+                Transformation1D::new(vec!['t','e']),
+                Transformation1D::new(vec![' ', 'H']),
+                Match::Linear, None),
+            Rule::new(
+                Transformation1D::new(vec!['s']),
+                Transformation1D::new(vec!['e']),
+                Match::Linear, None),
+            Rule::new(
+                Transformation1D::new(vec!['t']),
+                Transformation1D::new(vec!['l']),
+                Match::Linear, None),
+            Rule::new(
+                Transformation1D::new(vec!['i']),
+                Transformation1D::new(vec!['l']),
+                Match::Linear, None),
+            Rule::new(
+                Transformation1D::new(vec!['n','g']),
+                Transformation1D::new(vec!['o', ' ']),
+                Match::Linear, None),
+        ];
+
+        let mut engine = MarkovEngine::new(map, rules);
+        engine.finish();
+
+        assert_eq!(engine.state.state, [' ', 'H', 'e', 'l', 'l', 'o', ' ']);
+    }
 }
